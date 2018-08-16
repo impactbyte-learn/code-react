b4d69e982b977199c4bf1cfc5a47e42cc82916a9
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-16 14:36:06 +0700

    Refactor components

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import logo from './logo.svg'
+
+const Header = () => (
+  <header className="App-header">
+    <h1 className="App-title">Welcome to React</h1>
+  </header>
+)
+
+export default Header
---
+++ src/Paragraph.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Paragraph = () => <p className="App-intro">Hello friends.</p>
+
+export default Paragraph
